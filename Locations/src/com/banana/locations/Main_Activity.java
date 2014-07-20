265c67651dafd209b7a1dfbc7d8cf272e2b6f20b
akifhatipoglu <akifhatipoglu.tech@gmail.com>
2014-07-20 10:37:57 +0300

    isimlendirme degisti

--- Locations/src/com/banana/locations/Main_Activity.java
+++ Locations/src/com/banana/locations/Main_Activity.java
@@ -6,15 +6,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class HelloWorldActivity extends Activity {
+public class Main_Activity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        TextView text = new TextView(this);
-        text.setText("Hello World, Android - mkyong.com");
-        setContentView(text);
         setContentView(R.layout.main);
+        
+        
+        
     }
 }
