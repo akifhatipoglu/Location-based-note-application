56c35917fc83be6bad6512814af39e5fea982965
akifhatipoglu <akifhatipoglu.tech@gmail.com>
2014-07-20 16:29:08 +0300

    UI is done.

--- Locations/gen/com/mkyong/android/R.java
+++ Locations/gen/com/mkyong/android/R.java
@@ -11,7 +11,31 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int atm=0x7f020000;
+        public static final int bag=0x7f020001;
+        public static final int book=0x7f020002;
+        public static final int event=0x7f020003;
+        public static final int farstlights=0x7f020004;
+        public static final int gray=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int monologue=0x7f020007;
+        public static final int onixjam=0x7f020008;
+        public static final int pharmacy=0x7f020009;
+        public static final int shoppingcart=0x7f02000a;
+        public static final int wallpaper=0x7f02000b;
+    }
+    public static final class id {
+        public static final int imageButton1=0x7f050001;
+        public static final int imageButton2=0x7f050002;
+        public static final int imageButton3=0x7f050004;
+        public static final int imageButton4=0x7f050006;
+        public static final int imageButton5=0x7f050008;
+        public static final int imageButton6=0x7f05000a;
+        public static final int textView1=0x7f050000;
+        public static final int textView2=0x7f050003;
+        public static final int textView3=0x7f050005;
+        public static final int textView4=0x7f050007;
+        public static final int textView5=0x7f050009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
